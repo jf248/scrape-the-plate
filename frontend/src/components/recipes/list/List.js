3a483c72f7703771f6e76cb3b75e6410406b5f11
Joshua Freedman <jf248@users.noreply.github.com>
2018-07-10 13:23:49 -0700

    Change List to refresh on login

--- frontend/src/components/recipes/list/List.js
+++ frontend/src/components/recipes/list/List.js
@@ -1,17 +1,47 @@
 import React from 'react';
 
+import { Compose, renderProps } from 'lib/react-powerplug';
 import { RecordsMany } from 'lib/crud';
+import { Auth } from 'lib/auth';
 
 import ListPres from './ListPres';
 
+// TODO: This is an ugly hack to force a goFetch call to referesh the
+// RecordsMany component when user logs in.
+class Updater extends React.Component {
+  componentDidUpdate(prevProps) {
+    const { isLoggedIn, goFetch } = this.props;
+    if (isLoggedIn !== prevProps.isLoggedIn) {
+      goFetch();
+    }
+  }
+  render() {
+    return renderProps(this.props, {});
+  }
+}
+
 function List() {
-  const renderFunc = ({ ids, data, total, params, goFetch }) => {
+  const renderFunc = recordsMany => {
+    const { ids, data, total, params, goFetch } = recordsMany;
     const setPage = page => goFetch({ page });
     const setFilter = filter => goFetch({ filter });
     return <ListPres {...{ setPage, setFilter, ids, data, total, params }} />;
   };
 
-  return <RecordsMany resource={'recipes'} render={renderFunc} />;
+  return (
+    /* eslint-disable react/jsx-key */
+    <Compose
+      components={[
+        <RecordsMany fetchOnUpdate resource={'recipes'} render={renderFunc} />,
+        <Auth />,
+        (render, { goFetch }, { isLoggedIn }) => (
+          <Updater {...{ goFetch, isLoggedIn, render }} />
+        ),
+      ]}
+      render={renderFunc}
+    />
+    /* eslint-enable react/jsx-key */
+  );
 }
 
 export default List;
