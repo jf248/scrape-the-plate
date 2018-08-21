c6b13dfea66f0d2675a7d31c7b18f12574c77d39
Joshua Freedman <jf248@users.noreply.github.com>
2018-08-20 17:24:36 -0700

    Fix auth urls

--- frontend/src/config/authProvider.js
+++ frontend/src/config/authProvider.js
@@ -4,8 +4,8 @@ import { enhancedFetch } from './enhancedFetch';
 
 const URLS = {
   login: '/api/v1/auth/login/',
-  signup: '/api/v1/auth/login/',
-  verify: '/api/v1/auth/login/',
+  signup: '/api/v1/auth/signup/',
+  verify: '/api/v1/auth/verify/',
 };
 
 export const authProvider = enhance.authFetch(URLS)(enhancedFetch);
