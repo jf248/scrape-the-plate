e51d7e6d234cb5fc4b8c9e18b042b3d602e7661d
Joshua Freedman <jf248@users.noreply.github.com>
2018-11-07 10:07:20 -0800

    Upgrade django etc version

--- recipes/filters.py
+++ recipes/filters.py
@@ -1,4 +1,3 @@
-from django.db.models import Q
 from django_filters import rest_framework as filters
 from rest_framework.filters import SearchFilter, OrderingFilter
 
@@ -23,34 +22,38 @@ class OrderingFilter(OrderingFilter):
     """
 
 
-class ListFilter(filters.Filter):
-    def __init__(self, integer=False, **kwargs):
-        super(ListFilter, self).__init__(**kwargs)
-        if integer:
-            self.filter_value_fn = lambda x: int(x)
-        else:
-            self.filter_value_fn = lambda x: x
+# class ListFilter(filters.Filter):
+#     def __init__(self, integer=False, **kwargs):
+#         super(ListFilter, self).__init__(**kwargs)
+#         if integer:
+#             self.filter_value_fn = lambda x: int(x)
+#         else:
+#             self.filter_value_fn = lambda x: x
+#
+#     def sanitize(self, value_list):
+#         return [v for v in value_list if v != ""]
+#
+#     def filter(self, qs, value):
+#         values = value.split(",")
+#         values = self.sanitize(values)
+#         f = Q()
+#         for v in values:
+#             kwargs = {self.field_name: v}
+#             f = f | Q(**kwargs)
+#         return qs.filter(f)
 
-    def sanitize(self, value_list):
-        return [v for v in value_list if v != ""]
 
-    def filter(self, qs, value):
-        values = value.split(",")
-        values = self.sanitize(values)
-        f = Q()
-        for v in values:
-            kwargs = {self.name: v}
-            f = f | Q(**kwargs)
-        return qs.filter(f)
+class NumberInFilter(filters.BaseInFilter, filters.NumberFilter):
+    pass
 
 
-# class NumberInFilter(filters.BaseInFilter, filters.CharFilter):
-#     pass
+class CharInFilter(filters.BaseInFilter, filters.CharFilter):
+    pass
 
 
 class GroceryGroupFilter(filters.FilterSet):
-    id = ListFilter(name='id')
-    name = ListFilter(name='name')
+    id = NumberInFilter(field_name='id')
+    name = CharInFilter(field_name='name')
 
     class Meta:
         model = models.GroceryGroup
@@ -58,9 +61,9 @@ class GroceryGroupFilter(filters.FilterSet):
 
 
 class RecipeFilter(filters.FilterSet):
-    id = ListFilter(name='id')
-    user = ListFilter(name='user')
-    tags = ListFilter(name='tags')
+    id = NumberInFilter(field_name='id')
+    user = NumberInFilter(field_name='user')
+    tags = NumberInFilter(field_name='tags')
 
     class Meta:
         model = models.Recipe
